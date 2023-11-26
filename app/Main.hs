571b1e3daf597303950162e260fad5e1f646f462
TrueBoxGuy <17554188+TrueBoxGuy@users.noreply.github.com>
2023-11-26 12:30:30 +0000

    Reintroduce folder selection

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Control.Monad.Except 
+import Control.Monad.Except
 import Data.Foldable
 import System.IO
 import Text.Megaparsec
@@ -13,9 +13,8 @@ import Syntax
 
 main :: IO ()
 main = do
-    -- putStr "Folder name: "
-    -- hFlush stdout
-    -- dir <- getLine 
-    let dir = "Working"
-    res <- runExceptT (input dir >>= liftEither . (toMainStatements >=> topologicalSort >=> introTypes) )
+    putStr "Folder name: "
+    hFlush stdout
+    dir <- getLine
+    res <- runExceptT (input dir >>= liftEither . (toMainStatements >=> topologicalSort >=> introTypes))
     either (putStrLn . errorBundlePretty) (pure $ putStrLn "All definitions accepted") res
